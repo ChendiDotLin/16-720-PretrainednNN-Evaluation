8dc62d6ffbf70b524b58647b914becf08d512fd0
Turtle Lin <chendilin92@gmail.com>
2018-12-02 10:24:45 -0500

    three nets

--- toMat.py
+++ toMat.py
@@ -2,4 +2,10 @@ import numpy as np
 import scipy.io as io
 
 resnet_data = np.load('resnet50.npz')
+alexnet_data = np.load('alexnet.npz')
+squeezenet_data = np.load('squeezenet.npz')
+
 io.savemat('resnet.mat', mdict=resnet_data)
+io.savemat('alexnet.mat', mdict=alexnet_data)
+io.savemat('squeezenet.mat', mdict=squeezenet_data)
+
